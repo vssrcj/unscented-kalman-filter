45f87c59fe7cd26d8843d9326b08767602920bad
CJ <vssrcj@gmail.com>
2018-02-06 23:09:31 +0200

    initial small additions

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -6,6 +6,7 @@ using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using std::vector;
+#define EPS 0.0001 // A very small number
 
 /**
  * Initializes Unscented Kalman filter
@@ -45,7 +46,7 @@ UKF::UKF() {
 
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
-  //DO NOT MODIFY measurement noise values above these are provided by the sensor manufacturer.
+  // DO NOT MODIFY measurement noise values above these are provided by the sensor manufacturer.
   
   /**
   TODO:
@@ -54,6 +55,24 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  // set state dimension
+  n_x_ = 5;
+
+  // set augmented dimension±±
+  n_aug_ = 7;
+
+  is_initialized_ = false;
+
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+
+  weights_ = VectorXd(2 * n_aug_ + 1);
+
+  // define spreading parameter
+  lambda_ = 3 - n_aug_;
+
+  // Start time
+  time_us_ = 0;
 }
 
 UKF::~UKF() {}
@@ -69,6 +88,90 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+    /**
+      * Initialize the state ekf_.x_ with the first measurement.
+      * Create the covariance matrix.
+      * Remember: you'll need to convert radar from polar to cartesian coordinates.
+    */
+  
+    // Noise matrices
+    MatrixXd R_radar = MatrixXd(3,3);
+    MatrixXd R_laser = MatrixXd(2,2);
+
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      float rho     = meas_package.raw_measurements_(0);
+      float phi     = meas_package.raw_measurements_(1);
+      float rho_dot = meas_package.raw_measurements_(2);
+    
+      x_ << rho*cos(phi), rho*sin(phi), 0, rho_dot*cos(phi), rho_dot*sin(phi);
+
+      //state covariance matrix
+      //***** values can be tuned *****
+      P_ << std_radr_*std_radr_, 0, 0, 0, 0,
+            0, std_radr_*std_radr_, 0, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 0, std_radphi_, 0,
+            0, 0, 0, 0, std_radphi_;
+      
+      // Create R for update noise later
+      R_radar << std_radr_*std_radr_, 0, 0,
+                 0, std_radphi_*std_radphi_, 0,
+                 0, 0, std_radrd_*std_radrd_;
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state.
+      */
+      float x = meas_package.raw_measurements_(0);
+      float y = meas_package.raw_measurements_(1);
+
+      x_ << x, y, 0.f, 0.f, 0.f;
+
+      //state covariance matrix
+      //***** values can be tuned *****
+      P_ << std_laspx_*std_laspx_, 0, 0, 0, 0,
+            0, std_laspy_*std_laspy_, 0, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 0, 1, 0,
+            0, 0, 0, 0, 1;
+      
+      // Create R for update noise later
+      R_laser << std_laspx_*std_laspx_, 0,
+                 0, std_laspy_*std_laspy_;
+    }
+    // Deal with the special case initialisation problems
+    if (fabs(x_(0)) < EPS and fabs(x_(1)) < EPS) {
+      x_(0) = EPS;
+      x_(1) = EPS;
+    }
+
+    time_us_ = meas_package.timestamp_;
+
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+
+  //compute the time elapsed between the current and previous measurements
+	float dt = (meas_package.timestamp_ - time_us_) / 1000000.0;	//dt - expressed in seconds
+	time_us_ = meas_package.timestamp_;
+
+   // Predict
+  Prediction(dt);
+
+  // Measurement updates
+  if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    UpdateRadar(meas_package);
+  } else {
+    UpdateLidar(meas_package);
+  }
 }
 
 /**
